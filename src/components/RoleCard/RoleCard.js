9bd38070a6d510ab77d45116a327a8a892dcdc52
Jay Honnold <jayhonnold@gmail.com>
2019-09-20 06:29:51 -0500

    temp cards

--- src/components/RoleCard/RoleCard.js
+++ src/components/RoleCard/RoleCard.js
@@ -4,7 +4,27 @@ import classnames from 'classnames';
 
 import styles from './styles.module.scss';
 
-export default ({ team, role, message }) => {
+import Assassin from '../../assets/images/Assassin.svg';
+import Merlin from '../../assets/images/Merlin.svg';
+import Mordred from '../../assets/images/Mordred.svg';
+import Morgana from '../../assets/images/Morgana.svg';
+import Oberon from '../../assets/images/Oberon.svg';
+import Percival from '../../assets/images/Percival.svg';
+import VanillaEvil from '../../assets/images/Vanilla Evil.svg';
+import VanillaGood from '../../assets/images/Vanilla Good.svg';
+
+const imgMap = {
+	'Assassin': Assassin,
+	'Merlin': Merlin,
+	'Mordred': Mordred,
+	'Oberon': Oberon,
+	'Percival': Percival,
+	'Vanilla Evil': VanillaEvil,
+	'Vanilla Good': VanillaGood,
+	'Morgana': Morgana,
+};
+
+export default ({ team, role, knowledge = {} }) => {
 	const isEvil = /evil/i.test(team);
 
 	const className = classnames({
@@ -15,8 +35,16 @@ export default ({ team, role, message }) => {
 
 	return (
 		<div className={className}>
-			<p className={styles.role}>{role}</p>
-			<p className={styles.message}>{message}</p>
+			<img src={imgMap[role]} alt={role} />
+			<p className={styles.message}>
+				<ul>
+					{Object.keys(knowledge).map(k => (
+						<li key={k}>
+							<span>{k}: <strong>{knowledge[k]}</strong></span>
+						</li>
+					))}
+				</ul>
+			</p>
 		</div>
 	);
 };
